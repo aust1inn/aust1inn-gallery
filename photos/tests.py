557db7c10d61d29ee24a3436b0283f3177d4467e
aust1inn <austinotieno364@gmail.com>
2022-05-27 12:32:29 +0300

    moves the templates folder

--- photos/tests.py
+++ photos/tests.py
@@ -5,10 +5,10 @@ from .models import Image, Category, Location
 
 class TestImage(TestCase):
     def setUp(self):
-        self.location = Location(name='Moringa')
+        self.location = Location(name='Cool')
         self.location.save_location()
 
-        self.category = Category(name='home')
+        self.category = Category(name='nature')
         self.category.save_category()
 
         self.image_test = Image(id=1, name='image', description='this is a test image', location=self.location,
