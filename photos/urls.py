fe6bd789c3f1ddfa4dc0acc0b09de831591613a8
aust1inn <austinotieno364@gmail.com>
2022-05-28 14:12:32 +0300

    setting up urlconf for photos

---
+++ photos/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls import url
+from django.conf import settings
+from django.conf.urls.static import static
+from .import views
+
+# Create your views here.
+
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'^search/', views.search_results, name='search'),
+    url(r'^location/(?P<location>\w+)/', views.location_img, name='location'),
+]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- photos/views.py
+++ photos/views.py
@@ -1,3 +1,16 @@
+from django.conf.urls import url
 from django.shortcuts import render
+from django.conf import settings
+from django.conf.urls.static import static
+from .import views
 
 # Create your views here.
+
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'^search/', views.search_results, name='search'),
+    url(r'^location/(?P<location>\w+)/', views.location_img, name='location'),
+]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
