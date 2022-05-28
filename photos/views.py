4b3576ae10cd0545a424f69a4ce44be3e891ab74
aust1inn <austinotieno364@gmail.com>
2022-05-28 14:32:58 +0300

    defines location_img and search_results routes

--- photos/views.py
+++ photos/views.py
@@ -5,3 +5,18 @@ def index(request):
     images = Image.objects.all()
     locations = Location.get_locations()
     return render(request, 'photos/index.html', {'images': images[::-1], 'locations': locations})
+
+
+def search_results(request):
+    if 'imagesearch' in request.GET and request.GET["imagesearch"]:
+        category = request.GET.get("imagesearch")
+        searched_images = Image.search_by_category(category)
+        message = f"{category}"
+        return render(request, 'photos/search_results.html', {"message": message, "images": searched_images})
+    else:
+        message = "You haven't searched for any image category"
+        return render(request, 'photos/search_results.html', {"message": message})
+
+def location_img(request, location):
+    images = Image.filter_by_location(location)
+    return render(request, 'photos/images.html', {'location_images': images})
